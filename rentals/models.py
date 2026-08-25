e36e454e73e5aae3ff710117a9be95df4e1ccc9e
Anvith433 <anvithshetty932@gmail.com>
2026-08-25 09:48:20 +0100

    Add House model

---
+++ rentals/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class House(models.Model):
+
+    title = models.CharField(max_length=200)
+
+    location = models.CharField(max_length=100)
+
+    rent = models.IntegerField()
+
+    bedrooms = models.IntegerField()
+
+    bathrooms = models.IntegerField()
+
+    furnished = models.BooleanField(default=False)
+
+    parking = models.BooleanField(default=False)
+
+    area_sqft = models.IntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
